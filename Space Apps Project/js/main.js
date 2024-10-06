2d0857e0b840b2c54b1bdac60f63032216b01e67
brinto-006 <brinto06.bhattacharjee@outlook.com>
2024-10-06 10:45:24 +0600

    Add files via upload

---
+++ Space Apps Project/js/main.js
@@ -0,0 +1,15 @@
+$('.grid').isotope({
+		  
+	itemSelector: '.grid-item',
+	layoutMode: 'masonry'
+  });
+
+  // init Isotope
+  var $grid = $('.grid').isotope({
+	// options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+	var filterValue = $(this).attr('data-filter');
+	$grid.isotope({ filter: filterValue });
+  });
